4383d76414fad8c5ab0e9e05260410ecbda21d31
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2024-02-09 15:37:33 -0300

    feat: adiciona validação de envio de formulario e zoom no mapa

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,10 @@
 let nome = window.document.getElementById("nome");
 let email = document.querySelector("#email");
 let assunto = document.querySelector("#assunto");
+let nomeOk = false;
+let emailOk = false;
+let assuntoOk = false;
+let mapa = document.querySelector("#mapa");
 
 nome.style.width = "100%";
 email.style.width = "100%";
@@ -18,6 +22,7 @@ function validaNome() {
   } else {
     txtNome.innerHTML = "Nome válido";
     txtNome.style.color = "green";
+    nomeOk = true;
   }
 }
 
@@ -30,5 +35,38 @@ function validaEmail() {
   } else {
     txtEmail.innerHTML = "Email válido";
     txtEmail.style.color = "green";
+    emailOk = true;
   }
+}
+
+function validaAssunto() {
+  let txtAssunto = document.querySelector("#txtAssunto");
+
+  if (assunto.value.length >= 100) {
+    txtAssunto.innerHTML =
+      "Texto é muito grande, digite no máximo 100 caracteres";
+    txtAssunto.style.color = "red";
+    txtAssunto.style.display = "block";
+  } else {
+    txtAssunto.style.display = "none";
+    assuntoOk = true;
+  }
+}
+
+function enviar() {
+  if (nomeOk == true && emailOk == true && assuntoOk == true) {
+    alert("Formulário enviado com sucesso");
+  } else {
+    alert("Preencha o formulário corretamente");
+  }
+}
+
+function mapaZoom() {
+  mapa.style.width = "800px";
+  mapa.style.height = "600px";
+}
+
+function mapaNormal() {
+    mapa.style.width = "400px";
+    mapa.style.height = "250px";
 }
